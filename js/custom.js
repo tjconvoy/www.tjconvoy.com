f1e293accf878dca9679d01f1b07ba2ac9d3a511
tjconvoy <admin@tjconvoy.com>
2026-01-30 11:22:39 +0800

    fix code err

--- js/custom.js
+++ js/custom.js
@@ -1,89 +1,90 @@
 /*---------------------------------------------------------------------
-    File Name: custom.js
+	File Name: custom.js
 ---------------------------------------------------------------------*/
 
 $(function () {
-	
+
 	"use strict";
-	
+
 	/* Preloader
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
+
 	setTimeout(function () {
 		$('.loader_bg').fadeToggle();
 	}, 1500);
-	
+
 	/* Tooltip
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(document).ready(function(){
+
+	$(document).ready(function () {
 		$('[data-toggle="tooltip"]').tooltip();
 	});
-	
-	
-	
+
+
+
 	/* Mouseover
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(document).ready(function(){
-		$(".main-menu ul li.megamenu").mouseover(function(){
-			if (!$(this).parent().hasClass("#wrapper")){
-			$("#wrapper").addClass('overlay');
+
+	$(document).ready(function () {
+		$(".main-menu ul li.megamenu").mouseover(function () {
+			if (!$(this).parent().hasClass("#wrapper")) {
+				$("#wrapper").addClass('overlay');
 			}
 		});
-		$(".main-menu ul li.megamenu").mouseleave(function(){
+		$(".main-menu ul li.megamenu").mouseleave(function () {
 			$("#wrapper").removeClass('overlay');
 		});
 	});
-	
-	
-	
 
-	
-	
+
+
+
+
+
 	/* Toggle sidebar
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-     
-     $(document).ready(function () {
-       $('#sidebarCollapse').on('click', function () {
-          $('#sidebar').toggleClass('active');
-          $(this).toggleClass('active');
-       });
-     });
 
-     /* Product slider 
-     -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-     // optional
-     $('#blogCarousel').carousel({
-        interval: 5000
-     });
+	$(document).ready(function () {
+		$('#sidebarCollapse').on('click', function () {
+			$('#sidebar').toggleClass('active');
+			$(this).toggleClass('active');
+		});
+	});
+
+	/* Product slider 
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	// optional
+	$('#blogCarousel').carousel({
+		interval: 5000
+	});
 
 
 });
 
-$("select").on("click" , function() {
-  
-  $(this).parent(".select-box").toggleClass("open");
-  
+$("select").on("click", function () {
+
+	$(this).parent(".select-box").toggleClass("open");
+
 });
 
-$(document).mouseup(function (e)
-{
-    var container = $(".select-box");
+$(document).mouseup(function (e) {
+	var container = $(".select-box");
 
-    if (container.has(e.target).length === 0)
-    {
-        container.removeClass("open");
-    }
+	if (container.has(e.target).length === 0) {
+		container.removeClass("open");
+	}
 });
 
+function getURL() {
+	window.location.href;
+}
 
-$("select").on("change" , function() {
-  
-  var selection = $(this).find("option:selected").text(),
-      labelFor = $(this).attr("id"),
-      label = $("[for='" + labelFor + "']");
-    
-  label.find(".label-desc").html(selection);
-    
+$("select").on("change", function () {
+
+	var selection = $(this).find("option:selected").text(),
+		labelFor = $(this).attr("id"),
+		label = $("[for='" + labelFor + "']");
+
+	label.find(".label-desc").html(selection);
+
 });
